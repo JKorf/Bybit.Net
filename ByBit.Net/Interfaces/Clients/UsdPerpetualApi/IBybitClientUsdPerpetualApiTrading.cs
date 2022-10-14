0695fa8fb5878ea90f1f57bbcd289a581d6faa1e
kulikov-dev <kulikov.dev@gmail.com>
2022-10-14 09:24:45 +0600

    Issue 38: https://github.com/JKorf/Bybit.Net/issues/38

--- ByBit.Net/Interfaces/Clients/UsdPerpetualApi/IBybitClientUsdPerpetualApiTrading.cs
+++ ByBit.Net/Interfaces/Clients/UsdPerpetualApi/IBybitClientUsdPerpetualApiTrading.cs
@@ -171,7 +171,7 @@ namespace Bybit.Net.Interfaces.Clients.UsdPerpetualApi
         /// <param name="receiveWindow">The receive window for which this request is active. When the request takes longer than this to complete the server will reject the request</param>
         /// <param name="ct">Cancellation token</param>
         /// <returns></returns>
-        Task<WebCallResult<IEnumerable<BybitConditionalOrder>>> GetOpenConditionalOrdersRealTimeAsync(string symbol, long? receiveWindow = null, CancellationToken ct = default);
+        Task<WebCallResult<IEnumerable<BybitConditionalOrderUsd>>> GetOpenConditionalOrdersRealTimeAsync(string symbol, long? receiveWindow = null, CancellationToken ct = default);
 
         /// <summary>
         /// Get conditional order information. Either stopOrderId or clientOrderId should be provided
@@ -183,7 +183,7 @@ namespace Bybit.Net.Interfaces.Clients.UsdPerpetualApi
         /// <param name="receiveWindow">The receive window for which this request is active. When the request takes longer than this to complete the server will reject the request</param>
         /// <param name="ct">Cancellation token</param>
         /// <returns></returns>
-        Task<WebCallResult<BybitConditionalOrder>> GetOpenConditionalOrderRealTimeAsync(string symbol, string? stopOrderId = null, string? clientOrderId = null, long? receiveWindow = null, CancellationToken ct = default);
+        Task<WebCallResult<BybitConditionalOrderUsd>> GetOpenConditionalOrderRealTimeAsync(string symbol, string? stopOrderId = null, string? clientOrderId = null, long? receiveWindow = null, CancellationToken ct = default);
 
         /// <summary>
         /// Get a list of conditional orders
