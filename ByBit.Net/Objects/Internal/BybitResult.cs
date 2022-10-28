c512530ab45c330aaa09fc25eab550ab63f90ce0
JKorf <jankorf91@gmail.com>
2022-10-28 21:44:36 +0200

    Fixed Spot V3 api error parsing

--- ByBit.Net/Objects/Internal/BybitResult.cs
+++ ByBit.Net/Objects/Internal/BybitResult.cs
@@ -8,14 +8,35 @@ namespace Bybit.Net.Objects.Internal
     {
         [JsonProperty("ret_code")]
         public int ReturnCode { get; set; }
+        [JsonProperty("retCode")]
+        private int ReturnCodeInternal
+        {
+            get => ReturnCode;
+            set => ReturnCode = value; 
+        }
+
         [JsonProperty("ret_msg")]
         public string ReturnMessage { get; set; } = string.Empty;
+        [JsonProperty("retMsg")]
+        private string ReturnMessageInternal
+        {
+            get => ReturnMessage;
+            set => ReturnMessage = value;
+        }
+
         [JsonProperty("ext_code")]
         public int? ExtCode { get; set; }
         [JsonProperty("ext_info")]
         public string? ExtInfo { get; set; }
         [JsonProperty("time_now"), JsonConverter(typeof(DateTimeConverter))]
         public DateTime Timestamp { get; set; }
+        [JsonProperty("time"), JsonConverter(typeof(DateTimeConverter))]
+        private DateTime TimestampInternal
+        {
+            get => Timestamp;
+            set => Timestamp = value;
+        }
+
         [JsonProperty("rate_limit_status")]
         public int? RateLimitStatus { get; set; }
         [JsonProperty("rate_limit_reset_ms"), JsonConverter(typeof(DateTimeConverter))]
