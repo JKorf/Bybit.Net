004477463d64f97ef3f6640f840c67b771a832d5
JKorf <jankorf91@gmail.com>
2023-12-02 18:29:48 +0100

    Added SelfMatchPrevention parameter PlaceOrder

---
+++ ByBit.Net/Enums/SelfMatchPreventionType.cs
@@ -0,0 +1,31 @@
+﻿using CryptoExchange.Net.Attributes;
+
+namespace Bybit.Net.Enums
+{
+    /// <summary>
+    /// Self match prevention type
+    /// </summary>
+    public enum SelfMatchPreventionType
+    {
+        /// <summary>
+        /// None
+        /// </summary>
+        [Map("None")]
+        None,
+        /// <summary>
+        /// Cancel maker
+        /// </summary>
+        [Map("CancelMaker")]
+        CancelMaker,
+        /// <summary>
+        /// Cancel taker
+        /// </summary>
+        [Map("CancelTaker")]
+        CancelTaker,
+        /// <summary>
+        /// Cancel both
+        /// </summary>
+        [Map("CancelBoth")]
+        CancelBoth
+    }
+}
