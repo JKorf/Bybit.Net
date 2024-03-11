6bc8b2601bcd835c621dc6a5d38f99a3ad77345d
JKorf <jankorf91@gmail.com>
2024-03-11 11:27:44 +0100

    Added V5Api.ExchangeData.GetLongShortRatioAsync endpoint

--- ByBit.Net/Enums/DataPeriod.cs
+++ ByBit.Net/Enums/DataPeriod.cs
@@ -1,4 +1,6 @@
-﻿namespace Bybit.Net.Enums
+﻿using CryptoExchange.Net.Attributes;
+
+namespace Bybit.Net.Enums
 {
     /// <summary>
     /// Period
@@ -8,26 +10,32 @@
         /// <summary>
         /// 5min
         /// </summary>
+        [Map("5min")]
         FiveMinutes,
         /// <summary>
         /// 15min
         /// </summary>
+        [Map("15min")]
         FifteenMinutes,
         /// <summary>
         /// 30min
         /// </summary>
+        [Map("30min")]
         ThirtyMinutes,
         /// <summary>
         /// 1h
         /// </summary>
+        [Map("1h")]
         OneHour,
         /// <summary>
         /// 4h
         /// </summary>
+        [Map("4h")]
         FourHours,
         /// <summary>
         /// 1d
         /// </summary>
+        [Map("1d")]
         OneDay
     }
 }
