e801ae6bbda6c0d5aa9f1f786dca4f6ddee801ec
JKorf <jankorf91@gmail.com>
2024-03-11 11:37:30 +0100

    Updated BybtApiKeyInfo model

---
+++ ByBit.Net/Enums/KycLevel.cs
@@ -0,0 +1,26 @@
+﻿using CryptoExchange.Net.Attributes;
+
+namespace Bybit.Net.Enums
+{
+    /// <summary>
+    /// KnowYourCustomer level
+    /// </summary>
+    public enum KycLevel
+    {
+        /// <summary>
+        /// Default level
+        /// </summary>
+        [Map("LEVEL_DEFAULT")]
+        Default,
+        /// <summary>
+        /// Level 1
+        /// </summary>
+        [Map("LEVEL_1")]
+        Level1,
+        /// <summary>
+        /// Level 2
+        /// </summary>
+        [Map("LEVEL_2")]
+        Level2
+    }
+}
