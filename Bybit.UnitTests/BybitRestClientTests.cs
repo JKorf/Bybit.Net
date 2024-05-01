bcc7d136bde29e2c48980f3dcce3e534ff268fe4
Jan Korf <jankorf91@gmail.com>
2024-05-01 20:41:40 +0200

    CryptoExchange.Net testing update (#210)

--- Bybit.UnitTests/BybitRestClientTests.cs
+++ Bybit.UnitTests/BybitRestClientTests.cs
@@ -1,13 +1,18 @@
-﻿using Bybit.Net.Clients;
+﻿using Bybit.Net;
+using Bybit.Net.Clients;
 using Bybit.Net.Objects.Internal;
+using CryptoExchange.Net.Authentication;
+using CryptoExchange.Net.Clients;
 using Newtonsoft.Json;
 using NUnit.Framework;
 using NUnit.Framework.Legacy;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using CryptoExchange.Net.Converters.JsonNet;
 
 namespace Bybit.UnitTests
 {
@@ -72,6 +77,57 @@ namespace Bybit.UnitTests
             ClassicAssert.IsNotNull(result.Error);
             Assert.That(result.Error!.Code == 400001);
             Assert.That(result.Error.Message == "Error occured");
+        }
+
+        [Test]
+        public void CheckSignatureExample1()
+        {
+            var authProvider = new BybitAuthenticationProvider(new ApiCredentials("XXXXXXXXXX", "XXXXXXXXXX"));
+            var client = (RestApiClient)new BybitRestClient().V5Api;
+
+            CryptoExchange.Net.Testing.TestHelpers.CheckSignature(
+                client,
+                authProvider,
+                HttpMethod.Get,
+                "",
+                (uriParams, bodyParams, headers) =>
+                {
+                    return headers["X-BAPI-SIGN"].ToString();
+                },
+                "B3A13CBD6F9DE0AD7ECE696645A98000281BE3558716624CFCA03DEBD2F24574",
+                new Dictionary<string, object>
+                {
+                    { "category", "option" },
+                    { "symbol", "BTC-29JUL22-25000-C" },
+                },
+                DateTimeConverter.ParseFromLong(1658384314791),
+                true,
+                false);
+        }
+
+        [Test]
+        public void CheckSignatureExample2()
+        {
+            var authProvider = new BybitAuthenticationProvider(new ApiCredentials("XXXXXXXXXX", "XXXXXXXXXX"));
+            var client = (RestApiClient)new BybitRestClient().V5Api;
+
+            CryptoExchange.Net.Testing.TestHelpers.CheckSignature(
+                client,
+                authProvider,
+                HttpMethod.Post,
+                "",
+                (uriParams, bodyParams, headers) =>
+                {
+                    return headers["X-BAPI-SIGN"].ToString();
+                },
+                "A04DEA33A62E644897B14C6FF458DB428B9C2424B5CB785C9586490CCACA24AF",
+                new Dictionary<string, object>
+                {
+                    { "category", "option" }
+                },
+                DateTimeConverter.ParseFromLong(1658385579423),
+                true,
+                false);
         }
     }
 }
