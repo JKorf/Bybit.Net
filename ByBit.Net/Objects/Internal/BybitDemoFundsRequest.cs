bcf39d67b4aab847220e32bc620f0b6597bc5e91
JKorf <jankorf91@gmail.com>
2025-07-14 20:28:06 +0200

    Fixed issue in restClient.V5Api.Account.RequestDemoFundsAsync request signing

---
+++ ByBit.Net/Objects/Internal/BybitDemoFundsRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Bybit.Net.Objects.Internal
+{
+    internal class BybitDemoFundsRequest
+    {
+        [JsonPropertyName("coin")]
+        public string Asset { get; set; } = string.Empty;
+
+        [JsonPropertyName("amountStr"), JsonConverter(typeof(DecimalStringWriterConverter))]
+        public decimal Quantity { get; set; }
+    }
+}
