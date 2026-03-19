6873c55061a83b2a701974df5935fa39fb15439b
Jkorf <jankorf91@gmail.com>
2026-03-19 16:35:31 +0100

    wip

--- ByBit.Net/BybitCredentials.cs
+++ ByBit.Net/BybitCredentials.cs
@@ -11,12 +11,18 @@ namespace Bybit.Net
     {
         internal CredentialPair Credential { get; set; }
 
+        /// <summary>
+        /// HMAC credentials
+        /// </summary>
         public HMACCredential? HMAC
         {
             get => Credential as HMACCredential;
             set { if (value != null) Credential = value; }
         }
 
+        /// <summary>
+        /// RSA credentials in XML format
+        /// </summary>
         public RSAXmlCredential? RSAXml
         {
             get => Credential as RSAXmlCredential;
@@ -24,6 +30,9 @@ namespace Bybit.Net
         }
 
 #if NETSTANDARD2_1_OR_GREATER || NET7_0_OR_GREATER
+        /// <summary>
+        /// RSA credentials in PEM/Base64 format
+        /// </summary>
         public RSAPemCredential? RSAPem
         {
             get => Credential as RSAPemCredential;
@@ -31,6 +40,57 @@ namespace Bybit.Net
         }
 #endif
 
+        /// <summary>
+        /// Create new credentials
+        /// </summary>
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        public BybitCredentials() { }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+        /// <summary>
+        /// Create new credentials providing HMAC credentials
+        /// </summary>
+        /// <param name="key">API key</param>
+        /// <param name="secret">API secret</param>
+        public BybitCredentials(string key, string secret)
+        {
+            Credential = new HMACCredential(key, secret);
+        }
+
+        /// <summary>
+        /// Create new credentials providing HMAC credentials
+        /// </summary>
+        /// <param name="credential">HMAC Credentials</param>
+        public BybitCredentials(HMACCredential credential)
+        {
+            Credential = credential;
+        }
+
+        /// <summary>
+        /// Create new credentials providing RSA credentials in XML format
+        /// </summary>
+        /// <param name="credential">RSA credentials in XML format</param>
+        public BybitCredentials(RSAXmlCredential credential)
+        {
+            Credential = credential;
+        }
+
+#if NETSTANDARD2_1_OR_GREATER || NET7_0_OR_GREATER
+        /// <summary>
+        /// Create new credentials providing RSA credentials in PEM/Base64 format
+        /// </summary>
+        /// <param name="credential">RSA credentials in PEM/Base64 format</param>
+        public BybitCredentials(RSAPemCredential credential)
+        {
+            Credential = credential;
+        }
+#endif
+
+        /// <summary>
+        /// Specify the HMAC credentials
+        /// </summary>
+        /// <param name="key">API key</param>
+        /// <param name="secret">API secret</param>
         public BybitCredentials WithHMAC(string key, string secret)
         {
             if (Credential != null) throw new InvalidOperationException("Credentials already set");
@@ -39,6 +99,11 @@ namespace Bybit.Net
             return this;
         }
 
+        /// <summary>
+        /// Specify the RSA credentials in XML format
+        /// </summary>
+        /// <param name="key">API key</param>
+        /// <param name="privateKey">Private key</param>
         public BybitCredentials WithRSAXml(string key, string privateKey)
         {
             if (Credential != null) throw new InvalidOperationException("Credentials already set");
@@ -48,6 +113,11 @@ namespace Bybit.Net
         }
 
 #if NETSTANDARD2_1_OR_GREATER || NET7_0_OR_GREATER
+        /// <summary>
+        /// Specify the RSA credentials in PEM/Base64 format
+        /// </summary>
+        /// <param name="key">API key</param>
+        /// <param name="privateKey">Private key</param>
         public BybitCredentials WithRSAPem(string key, string privateKey)
         {
             if (Credential != null) throw new InvalidOperationException("Credentials already set");
@@ -59,5 +129,14 @@ namespace Bybit.Net
 
         /// <inheritdoc />
         public override ApiCredentials Copy() => new BybitCredentials { Credential = Credential };
+
+        /// <inheritdoc />
+        public override void Validate()
+        {
+            if (Credential == null)
+                throw new ArgumentException("Credential not set");
+
+            Credential.Validate();
+        }
     }
 }
