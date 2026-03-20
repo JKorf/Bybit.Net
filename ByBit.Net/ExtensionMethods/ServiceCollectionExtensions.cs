2869c0c6b0355692253708c31d10ff9f1ab0670b
Jkorf <jankorf91@gmail.com>
2026-03-20 13:56:52 +0100

    wip

--- ByBit.Net/ExtensionMethods/ServiceCollectionExtensions.cs
+++ ByBit.Net/ExtensionMethods/ServiceCollectionExtensions.cs
@@ -24,7 +24,7 @@ namespace Microsoft.Extensions.DependencyInjection
     {
         /// <summary>
         /// Add services such as the IBybitRestClient and IBybitSocketClient. Configures the services based on the provided configuration.<br />
-        /// See <see href="https://github.com/JKorf/CryptoExchange.Net/blob/master/Examples/example-config.json" /> for an example of how to set up the configuration.
+        /// See <see href="https://github.com/JKorf/Bybit.Net/blob/main/Examples/example-config.json" /> for an example of how to set up the configuration.
         /// </summary>
         /// <param name="services">The service collection</param>
         /// <param name="configuration">The configuration(section) containing the options</param>
