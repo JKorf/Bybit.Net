8b6c4f1a33a61a13d6b3d1d7c1b252bc3febacfb
Jkorf <jankorf91@gmail.com>
2026-07-02 13:31:08 +0200

    Updated CryptoExchange.Net to V12.0.2, Updated integration tests to new warning support

--- Bybit.UnitTests/BybitRestIntegrationTests.cs
+++ Bybit.UnitTests/BybitRestIntegrationTests.cs
@@ -53,68 +53,77 @@ namespace Bybit.Net.UnitTests
         [Test]
         public async Task TestV5Account()
         {
-            await RunAndCheckResult(client => client.V5Api.Account.GetBalancesAsync(Enums.AccountType.Unified, default, default), true);
-            await RunAndCheckResult(client => client.V5Api.Account.GetBorrowHistoryAsync(default, default, default, default, default, default), true);
-            await RunAndCheckResult(client => client.V5Api.Account.GetCollateralInfoAsync(default, default), true);
-            await RunAndCheckResult(client => client.V5Api.Account.GetAssetGreeksAsync(default, default), true);
-            await RunAndCheckResult(client => client.V5Api.Account.GetFeeRateAsync(Enums.Category.Spot, default, default, default), true);
-            await RunAndCheckResult(client => client.V5Api.Account.GetMarginAccountInfoAsync(default), true);
-            await RunAndCheckResult(client => client.V5Api.Account.GetTransactionHistoryAsync(default, default, default, default, default, default, default, default, default,  default), true);
-            await RunAndCheckResult(client => client.V5Api.Account.GetAssetInfoAsync(default, default), true);
-            await RunAndCheckResult(client => client.V5Api.Account.GetWithdrawalsAsync(default, default, default, default, default, default, default, default, default), true);
-            await RunAndCheckResult(client => client.V5Api.Account.GetDelayedWithdrawQuantityAsync("ETH", default), true);
-            await RunAndCheckResult(client => client.V5Api.Account.GetApiKeyInfoAsync(default), true);
-            await RunAndCheckResult(client => client.V5Api.Account.GetAccountTypesAsync(default, default), true);
-            await RunAndCheckResult(client => client.V5Api.Account.GetSpotMarginStatusAndLeverageAsync(default), true);
-            await RunAndCheckResult(client => client.V5Api.Account.GetSpotMarginDataAsync(default, default, default), true);
-            await RunAndCheckResult(client => client.V5Api.Account.GetSpotMarginInterestRateHistoryAsync("ETH", default, default, default, default), true);
-            await RunAndCheckResult(client => client.V5Api.Account.GetConvertAssetsAsync(Enums.ConvertAccountType.ConvertUta, default, default, default), true);
-            await RunAndCheckResult(client => client.V5Api.Account.GetConvertHistoryAsync(Enums.ConvertAccountType.ConvertUta, default, default, default), true);
+            var warnings = new List<Exception>();
+            await RunAndCheckResult(warnings, client => client.V5Api.Account.GetBalancesAsync(Enums.AccountType.Unified, default, default), true, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.Account.GetBorrowHistoryAsync(default, default, default, default, default, default), true, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.Account.GetCollateralInfoAsync(default, default), true, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.Account.GetAssetGreeksAsync(default, default), true, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.Account.GetFeeRateAsync(Enums.Category.Spot, default, default, default), true, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.Account.GetMarginAccountInfoAsync(default), true, "result", ignoreProperties: ["spotHedgingStatus"]);
+            await RunAndCheckResult(warnings, client => client.V5Api.Account.GetTransactionHistoryAsync(default, default, default, default, default, default, default, default, default,  default), true, "result", ignoreProperties: ["extraFees"]);
+            await RunAndCheckResult(warnings, client => client.V5Api.Account.GetAssetInfoAsync(default, default), true, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.Account.GetWithdrawalsAsync(default, default, default, default, default, default, default, default, default), true, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.Account.GetDelayedWithdrawQuantityAsync("ETH", default), true, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.Account.GetApiKeyInfoAsync(default), true, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.Account.GetAccountTypesAsync(default, default), true, "result.accounts");
+            await RunAndCheckResult(warnings, client => client.V5Api.Account.GetSpotMarginStatusAndLeverageAsync(default), true, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.Account.GetSpotMarginDataAsync(default, default, default), true, "result.vipCoinList");
+            await RunAndCheckResult(warnings, client => client.V5Api.Account.GetSpotMarginInterestRateHistoryAsync("ETH", default, default, default, default), true, "result.list");
+            await RunAndCheckResult(warnings, client => client.V5Api.Account.GetConvertAssetsAsync(Enums.ConvertAccountType.ConvertUta, default, default, default), true, "result.coins");
+            await RunAndCheckResult(warnings, client => client.V5Api.Account.GetConvertHistoryAsync(Enums.ConvertAccountType.ConvertUta, default, default, default), true, "result.list");
 
             // Only available to broker account
             //await RunAndCheckResult(client => client.V5Api.Account.GetBrokerAccountInfoAsync(default), true);
             //await RunAndCheckResult(client => client.V5Api.Account.GetBrokerEarningsAsync(default, default, default, default, default, default, default), true);
+            foreach (var warning in warnings)
+                Assert.Warn(warning.Message);
         }
 
         [Test]
         public async Task TestV5ExchangeData()
         {
+            var warnings = new List<Exception>();
             await RunAndCheckResult(client => client.V5Api.ExchangeData.GetServerTimeAsync(default), false);
-            await RunAndCheckResult(client => client.V5Api.ExchangeData.GetKlinesAsync(Enums.Category.Spot, "ETHUSDT", Enums.KlineInterval.OneDay, default, default, default, default), false);
-            await RunAndCheckResult(client => client.V5Api.ExchangeData.GetMarkPriceKlinesAsync(Enums.Category.Linear, "ETHUSDT", Enums.KlineInterval.OneDay, default, default, default, default), false);
-            await RunAndCheckResult(client => client.V5Api.ExchangeData.GetIndexPriceKlinesAsync(Enums.Category.Linear, "ETHUSDT", Enums.KlineInterval.OneDay, default, default, default, default), false);
-            await RunAndCheckResult(client => client.V5Api.ExchangeData.GetPremiumIndexPriceKlinesAsync(Enums.Category.Linear, "ETHUSDT", Enums.KlineInterval.OneDay, default, default, default, default), false);
-            await RunAndCheckResult(client => client.V5Api.ExchangeData.GetSpotSymbolsAsync(default, default), false);
-            await RunAndCheckResult(client => client.V5Api.ExchangeData.GetOptionSymbolsAsync(default, default, default, default, default), false);
-            await RunAndCheckResult(client => client.V5Api.ExchangeData.GetLinearInverseSymbolsAsync(default, default, default, default, default, default, default, default), false);
-            await RunAndCheckResult(client => client.V5Api.ExchangeData.GetOrderbookAsync(Enums.Category.Spot, "ETHUSDT", default, default), false);
-            await RunAndCheckResult(client => client.V5Api.ExchangeData.GetSpotTickersAsync(default, default), false);
-            await RunAndCheckResult(client => client.V5Api.ExchangeData.GetOptionTickersAsync(default, "BTC", default, default), false);
-            await RunAndCheckResult(client => client.V5Api.ExchangeData.GetLinearInverseTickersAsync(Enums.Category.Linear, default, default, default, default), false);
-            await RunAndCheckResult(client => client.V5Api.ExchangeData.GetFundingRateHistoryAsync(Enums.Category.Linear, "ETHUSDT", default, default, default, default), false);
-            await RunAndCheckResult(client => client.V5Api.ExchangeData.GetTradeHistoryAsync(Enums.Category.Linear, "ETHUSDT", default, default, default, default), false);
-            await RunAndCheckResult(client => client.V5Api.ExchangeData.GetOpenInterestAsync(Enums.Category.Linear, "ETHUSDT", Enums.OpenInterestInterval.OneDay, default, default, default, default, default), false);
-            await RunAndCheckResult(client => client.V5Api.ExchangeData.GetHistoricalVolatilityAsync(default, default, default, default, default, default), false);
-            await RunAndCheckResult(client => client.V5Api.ExchangeData.GetInsuranceAsync("ETH", default), false);
-            await RunAndCheckResult(client => client.V5Api.ExchangeData.GetRiskLimitAsync(Enums.Category.Linear, default, default, default), false);
-            await RunAndCheckResult(client => client.V5Api.ExchangeData.GetDeliveryPriceAsync(Enums.Category.Linear, default, default, default, default, default, default), false);
-            await RunAndCheckResult(client => client.V5Api.ExchangeData.GetLongShortRatioAsync(Enums.Category.Linear, "ETHUSDT", Enums.DataPeriod.OneDay, default, default, default, default), false);
-            await RunAndCheckResult(client => client.V5Api.ExchangeData.GetOrderPriceLimitAsync("ETHUSDT", default, default), false);
+            await RunAndCheckResult(warnings, client => client.V5Api.ExchangeData.GetKlinesAsync(Enums.Category.Spot, "ETHUSDT", Enums.KlineInterval.OneDay, default, default, default, default), false, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.ExchangeData.GetMarkPriceKlinesAsync(Enums.Category.Linear, "ETHUSDT", Enums.KlineInterval.OneDay, default, default, default, default), false, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.ExchangeData.GetIndexPriceKlinesAsync(Enums.Category.Linear, "ETHUSDT", Enums.KlineInterval.OneDay, default, default, default, default), false, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.ExchangeData.GetPremiumIndexPriceKlinesAsync(Enums.Category.Linear, "ETHUSDT", Enums.KlineInterval.OneDay, default, default, default, default), false, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.ExchangeData.GetSpotSymbolsAsync(default, default), false, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.ExchangeData.GetOptionSymbolsAsync(default, default, default, default, default), false, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.ExchangeData.GetLinearInverseSymbolsAsync(default, default, default, default, default, default, default, default), false, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.ExchangeData.GetOrderbookAsync(Enums.Category.Spot, "ETHUSDT", default, default), false, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.ExchangeData.GetSpotTickersAsync(default, default), false, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.ExchangeData.GetOptionTickersAsync(default, "BTC", default, default), false, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.ExchangeData.GetLinearInverseTickersAsync(Enums.Category.Linear, default, default, default, default), false, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.ExchangeData.GetFundingRateHistoryAsync(Enums.Category.Linear, "ETHUSDT", default, default, default, default), false, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.ExchangeData.GetTradeHistoryAsync(Enums.Category.Linear, "ETHUSDT", default, default, default, default), false, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.ExchangeData.GetOpenInterestAsync(Enums.Category.Linear, "ETHUSDT", Enums.OpenInterestInterval.OneDay, default, default, default, default, default), false, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.ExchangeData.GetHistoricalVolatilityAsync(default, default, default, default, default, default), false, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.ExchangeData.GetInsuranceAsync("ETH", default), false, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.ExchangeData.GetRiskLimitAsync(Enums.Category.Linear, default, default, default), false, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.ExchangeData.GetDeliveryPriceAsync(Enums.Category.Linear, default, default, default, default, default, default), false, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.ExchangeData.GetLongShortRatioAsync(Enums.Category.Linear, "ETHUSDT", Enums.DataPeriod.OneDay, default, default, default, default), false, "result.list");
+            await RunAndCheckResult(warnings, client => client.V5Api.ExchangeData.GetOrderPriceLimitAsync("ETHUSDT", default, default), false, "result");
+            foreach (var warning in warnings)
+                Assert.Warn(warning.Message);
         }
 
         [Test]
         public async Task TestV5Trading()
         {
-            await RunAndCheckResult(client => client.V5Api.Trading.GetOrdersAsync(Enums.Category.Spot, default, default, default, default, default, default, default, default, default, default), true);
-            await RunAndCheckResult(client => client.V5Api.Trading.GetOrderHistoryAsync(Enums.Category.Spot, default, default, default, default, default, default, default, default, default, default, default), true);
-            await RunAndCheckResult(client => client.V5Api.Trading.GetBorrowQuotaAsync("ETHUSDT", Enums.OrderSide.Buy, default), true);
-            await RunAndCheckResult(client => client.V5Api.Trading.GetDisconnectCancelAllConfigAsync(default), true);
-            await RunAndCheckResult(client => client.V5Api.Trading.GetUserTradesAsync(Enums.Category.Spot, default, default, default, default, default, default, default, default, default, default, default), true);
-            await RunAndCheckResult(client => client.V5Api.Trading.GetPositionsAsync(Enums.Category.Linear, default, default, "USDT", default, default, default), true);
-            await RunAndCheckResult(client => client.V5Api.Trading.GetAssetExchangeHistoryAsync(default, default, default, default, default), true);
-            await RunAndCheckResult(client => client.V5Api.Trading.GetDeliveryHistoryAsync(Enums.Category.Linear, default, default, default, default, default, default, default), true);
-            await RunAndCheckResult(client => client.V5Api.Trading.GetSettlementHistoryAsync(Enums.Category.Linear, default, default, default, default, default, default), true);
-            await RunAndCheckResult(client => client.V5Api.Trading.GetClosedProfitLossAsync(Enums.Category.Linear, default, default, default, default, default, default), true);
+            var warnings = new List<Exception>();
+            await RunAndCheckResult(warnings, client => client.V5Api.Trading.GetOrdersAsync(Enums.Category.Spot, default, default, default, default, default, default, default, default, default, default), true, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.Trading.GetOrderHistoryAsync(Enums.Category.Spot, default, default, default, default, default, default, default, default, default, default, default), true, "result", ignoreProperties: ["extraFees"]);
+            await RunAndCheckResult(warnings, client => client.V5Api.Trading.GetBorrowQuotaAsync("ETHUSDT", Enums.OrderSide.Buy, default), true, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.Trading.GetDisconnectCancelAllConfigAsync(default), true, "result.dcpInfos");
+            await RunAndCheckResult(warnings, client => client.V5Api.Trading.GetUserTradesAsync(Enums.Category.Spot, default, default, default, default, default, default, default, default, default, default, default), true, "result", ignoreProperties: ["extraFees"]);
+            await RunAndCheckResult(warnings, client => client.V5Api.Trading.GetPositionsAsync(Enums.Category.Linear, default, default, "USDT", default, default, default), true, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.Trading.GetAssetExchangeHistoryAsync(default, default, default, default, default), true, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.Trading.GetDeliveryHistoryAsync(Enums.Category.Linear, default, default, default, default, default, default, default), true, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.Trading.GetSettlementHistoryAsync(Enums.Category.Linear, default, default, default, default, default, default), true, "result");
+            await RunAndCheckResult(warnings, client => client.V5Api.Trading.GetClosedProfitLossAsync(Enums.Category.Linear, default, default, default, default, default, default), true, "result");
+            foreach (var warning in warnings)
+                Assert.Warn(warning.Message);
         }
 
         [Test]
